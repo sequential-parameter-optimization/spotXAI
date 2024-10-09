c9863dd31a73f25755b94583837fcb36d0764d48
Alexander Hinterleitner <alexander.hinterleitner@smail.th-koeln.de>
2024-10-09 11:11:59 +0200

    update scatter plot

--- src/spotXAI/utils/visualization.py
+++ src/spotXAI/utils/visualization.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 
 def boxplot_attribution(attribution_distribution_df, size=(12, 8), attr_filter=None):
@@ -34,38 +35,54 @@ def boxplot_attribution(attribution_distribution_df, size=(12, 8), attr_filter=N
     plt.show()
 
 
-def scatter_attribution(attribution_distribution_df, size=(12, 8), attr_filter=None):
+def scatter_attribution(df, size=(12, 8), attr_filter=None, param_list = None):
     """
     Generate a scatter plot to visualize the attribution values for each feature.
 
     Parameters:
-    attribution_distribution_df (data frame): A data frame containing attribution value, feature index and corresponding y value.
+    df (data frame): A data frame containing attribution value, feature index and corresponding y value.
     size (tuple, optional): The size of the plot (width, height). Default is (12, 8).
     attr_filter (tuple, optional): The y-axis limits for the plot. Default is None.
+    param_list (list, optional): A list of the feature names.
 
     Returns:
     None
     """
-
-    # Create the scatter plot
-    fig, ax = plt.subplots(figsize=size)
-    scatter = sns.scatterplot(
-        x="feature index",
-        y="attribution value",
-        hue="corresponding y_value",
-        palette="viridis",
-        data=attribution_distribution_df,
-        ax=ax,
-        legend=False,
-    )
+    if param_list:
+        param_df = pd.DataFrame({'Index': range(len(param_list)), 'Parameter': param_list})
+        attribution_distribution_df = pd.merge(df, param_df, left_on='feature index', right_on='Index')
+        fig, ax = plt.subplots(figsize=size)
+        scatter = sns.scatterplot(
+            x="Parameter",
+            y="attribution value",
+            hue="corresponding y_value",
+            palette="viridis",
+            data=attribution_distribution_df,
+            ax=ax,
+            legend=False,
+        )
+    else:
+        attribution_distribution_df = df
+        fig, ax = plt.subplots(figsize=size)
+        scatter = sns.scatterplot(
+            x="feature index",
+            y="attribution value",
+            hue="corresponding y_value",
+            palette="viridis",
+            data=attribution_distribution_df,
+            ax=ax,
+            legend=False,
+        )
 
     if attr_filter is not None:
         ax.set_ylim(attr_filter)
 
     # Customize the plot
     plt.title("Scatter Plot of Attribution Values for Each Feature")
-    plt.xlabel("Feature Index")
+    plt.xlabel("Feature")
     plt.ylabel("Attribution Value")
+    if param_list:
+        plt.xticks(rotation=90)
 
     # Create a colorbar
     norm = plt.Normalize(
